89d322e21cdf84f1bdbe1615af4e61c9d97fef2b
nanachiki <nanachiki256@gmail.com>
2025-06-30 18:00:08 +0900

    feat: inplement the card component's logic to render pokemon's info

--- src/utils/components/Card/card.js
+++ src/utils/components/Card/card.js
@@ -1,10 +1,36 @@
 // rafce: component 1
 import React from 'react'
 
-const card = () => {
+const card = ({ pokemon}) => {
   return (
-    <div>
-      <h1>This is my first component!!!</h1>
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      </div>
+      <h3>{pokemon.name}</h3>
+      <div className="cardTypes">
+        <h4>{pokemon.types.length > 1 ? "Types" : "Type"}</h4>
+        {pokemon.types.map((type, i) => {
+          return (
+            <div key={i}><span className="typeName">{type.type.name}</span></div>
+          )
+        })}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">Id: {pokemon.id}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Weight: {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Height: {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Ability: {pokemon.abilities[0].ability.name}</p>
+        </div>
+        
+      </div>
     </div>
   )
 }
