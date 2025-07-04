fea5fda5acf20434d38e5a1e8f35c9f4572333bc
nanachiki <nanachiki256@gmail.com>
2025-07-04 23:32:34 +0900

    Add a next button to navigate to the next page that shows the next 20 pokemons

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 import Card from './utils/components/Card/card.js';
 import Navbar from './utils/components/Navbar/Navbar.js';
 import MyFooter from './utils/components/MyFooter/MyFooter.js';
+import handleNextPage from './utils/components/PNButtons/handleNextPage.js'
 
 function App() {
   // Initial Endpoint
@@ -13,13 +14,18 @@ function App() {
   const [loading, setLoading] = useState(true);
   // State to store each pokemon's detailed data
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState('');
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // Fetch all pokemon data till the end of the page
       let res = await getAllPokemon(initialURL);
+      
       console.log('All pokemon data : ', res);
       console.log('All pokemon data results : ', res.results);
+     
+      // Store the URL of the next 20 pokemon
+      setNextURL(res.next);
 
       // Fetch detailed data of each pokemon
       loadPokemon(res.results);
@@ -30,7 +36,6 @@ function App() {
 
   const loadPokemon = async (data) => {
     // Promise.all receives an array as an argument and ensures that all the data in the array is fetched
-
     const _pokemonData = await Promise.all(
       data.map((pokemon) => {
         // pokemon.url is the endpoint path to the pokemon's detailed data
@@ -40,8 +45,17 @@ function App() {
     );
     setPokemonData(_pokemonData); // Store the fetched data in state
   };
+
   // since _pokemonData is stored in state, we can access it outside of the loadPokemon function
   console.log('Detailed data 2: ', pokemonData);
+
+ const handleNextPage = async () => {
+  setLoading(true);
+  console.log(nextURL);
+  let nextData = await getAllPokemon(nextURL);
+  loadPokemon(nextData.results);
+  setLoading(false);
+ }
 
   return (
     <>
@@ -57,6 +71,8 @@ function App() {
             })}
           </div>
         )}
+      {/* <button onClick={handlePrevPage}>Previous</button> */}
+      <button onClick={handleNextPage}>Next</button>
       </div>
       <MyFooter />
     </>
